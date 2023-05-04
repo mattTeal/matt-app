fe00b4e0707597e49f34cfeb7f88841807296753
tattmeal <matt.tealcosc@gmail.com>
2023-05-04 17:29:42 -0500

    public url

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css'
 function App() {
   return (
     <div>
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route
             path='/'
